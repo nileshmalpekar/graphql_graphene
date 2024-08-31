02e89220cfad2b52d29c11278154f5c91de3a6f8
Nilesh Malpekar <nileshmalpekar@gmail.com>
2024-08-31 09:03:33 -0500

    test added

---
+++ test_query.py
@@ -0,0 +1,11 @@
+from graphene.test import Client
+from database import init_db
+from schema import schema
+
+init_db()
+
+client = Client(schema)
+
+def test_users_query():
+    result = client.execute("""query { users { firstName, lastName }}""")
+    assert result == {"data": {"users": [{"firstName": "Nilesh", "lastName": "Malpekar"}]}}
